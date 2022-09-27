e89b315f63510a12d21b32e16953727e89a005cf
Adarsh <adarshriit2017@gmail.com>
2022-09-28 03:09:28 +0530

    ci cd deployed

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Starting my Machine learing project."
+    return "CI/CD has been deployed successfully."
 
 if __name__=="__main__":
     app.run(debug=True) 
