cb8433b8c5a4e751cc7bbca4e15240f490920df2
Adarsh <adarshriit2017@gmail.com>
2022-10-08 05:03:27 +0530

    erros

--- housing/entity/artifact_entity.py
+++ housing/entity/artifact_entity.py
@@ -1,3 +1,7 @@
 from collections import namedtuple
 
 DataIngestionArtifact = namedtuple("DataIngestionArtifact",["is_ingested","train_file_path","test_file_path","message"])
+
+DataValidationArtifact = namedtuple("DataValidationArtifact",
+["schema_file_path","report_file_path","report_page_file_path","is_validated","message"])
+
